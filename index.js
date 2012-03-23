4922097d556ed8f2d2b362f214927b995765444d
Todd Wolfson <todd@twolfson.com>
2012-03-22 23:38:02 -0700

    JSON to HTML looking good

--- index.js
+++ index.js
@@ -1,44 +1,95 @@
 (function () {
-function Mason(htmlObj) {
+function Mason(htmlArr) {
+  // Create a document fragment (collection of HTMLElements) to return
   var retFrag = document.createDocumentFragment(),
-      div = document.createElement('div');
-  div.innerHTML = 'Hey!';
-  retFrag.appendChild(div);
+      node,
+      nodeType,
+      tagName,
+      elt,
+      childNodes,
+      childFrag,
+      i = 0,
+      len = htmlArr.length;
+
+  // Iterate each node in the HTML array
+  for (; i < len; i++) {
+    node = htmlArr[i];
+    nodeType = node.nodeType;
+
+    // Based on the type of the node
+    switch (nodeType) {
+      case 'tag':
+        tagName = node.tagName;
+        // TODO: If the tagName is in the map of modules
+        if (false) {
+          // Render it via that method
+        } else {
+        // Otherwise, create the element of the type
+          elt = document.createElement(tagName);
+
+          // If it has any children
+          childNodes = node.childNodes;
+          if (childNodes !== undefined) {
+            // Render the child nodes
+            childFrag = Mason(childNodes);
+
+            // and append them to this node
+            elt.appendChild(childFrag);
+          }
+        }
+        break;
+      case 'text':
+        // Create a text node
+        elt = document.createTextNode(node.textContent);
+        break;
+    }
+
+    // Append the element to the collection
+    retFrag.appendChild(elt);
+  }
+
+  // Return the rendered fragment
   return retFrag;
 }
 
 // Get the insertion area
 var insertArea = document.getElementById('insertArea'),
-    // Create an HTML object to render
+    // Create an array of "HTML elements" to render
     // TODO: A plaintext HTML interpretter will come later -- maybe borrowed from another project
-    preRenderHtml = [{
-      'menubutton': {
-        'childNodes': [{
-          'nodeType': 'tag', // TODO: Use real constant
-          'tagName': 'button',
-          'childNodes': [{
-            'nodeType': 'text',
-            'textContent': 'First button'
-          }]
-        }, {
-          'nodeType': 'tag',
-          'tagName': 'button',
-          'childNodes': [{
-            'nodeType': 'text',
-            'textContent': 'Second button'
-          }]
-        },{
-          'nodeType': 'tag',
-          'tagName': 'button',
-          'childNodes': [{
-            'nodeType': 'text',
-            'textContent': 'Third button'
-          }]
-        }]
-      }
+    htmlArr = [{
+      'nodeType': 'tag',
+      'tagName': 'button',
+      'childNodes': [{
+        'nodeType': 'text',
+        'textContent': 'I am a button!'
+      }]
+      // 'menubutton': {
+        // 'childNodes': [{
+          // 'nodeType': 'tag', // TODO: Use real constant
+          // 'tagName': 'button',
+          // 'childNodes': [{
+            // 'nodeType': 'text',
+            // 'textContent': 'First button'
+          // }]
+        // }, {
+          // 'nodeType': 'tag',
+          // 'tagName': 'button',
+          // 'childNodes': [{
+            // 'nodeType': 'text',
+            // 'textContent': 'Second button'
+          // }]
+        // },{
+          // 'nodeType': 'tag',
+          // 'tagName': 'button',
+          // 'childNodes': [{
+            // 'nodeType': 'text',
+            // 'textContent': 'Third button'
+          // }]
+        // }]
+      // }
     }],
-    // Render the JSON into a document fragment
-    htmlFrag = Mason(preRenderHtml);
+    // Render the HTML elements into a document fragment
+    htmlFrag = Mason(htmlArr);
 
 // Append the fragment
 // TODO: onexpand bindings, onclick binding for menu button as a whole [triggers will be set up by Mason]
