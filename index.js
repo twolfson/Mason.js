3381b06ab770077a405aaf408092a79eb5c7b39f
Todd Wolfson <todd@twolfson.com>
2012-03-22 23:57:31 -0700

    Proof of concept looking good

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 (function () {
+// TODO: Rename this to MasonFactory and move the static methods as prototypal methods
 function Mason(htmlArr) {
   // Create a document fragment (collection of HTMLElements) to return
   var retFrag = document.createDocumentFragment(),
@@ -9,7 +10,8 @@ function Mason(htmlArr) {
       childNodes,
       childFrag,
       i = 0,
-      len = htmlArr.length;
+      len = htmlArr.length,
+      modules = Mason.modules;
 
   // Iterate each node in the HTML array
   for (; i < len; i++) {
@@ -20,9 +22,11 @@ function Mason(htmlArr) {
     switch (nodeType) {
       case 'tag':
         tagName = node.tagName;
+
         // TODO: If the tagName is in the map of modules
-        if (false) {
+        if (modules.hasOwnProperty(tagName)) {
           // Render it via that method
+          elt = modules[tagName](node);
         } else {
         // Otherwise, create the element of the type
           elt = document.createElement(tagName);
@@ -51,6 +55,53 @@ function Mason(htmlArr) {
   // Return the rendered fragment
   return retFrag;
 }
+
+// Static properties/methods for Mason
+Mason.modules = {};
+
+// TODO: Accept objects via addModules 'sugar' method
+/**
+ * Add module method for Mason
+ * @param {Object} module Object containing key value pairs of tags and their respective functions
+ * @param {Function} module.* Function that will render a document fragment/HTMLElement. The key that this is stored under will affect what tags it renders to.
+ */
+Mason.addModule = function (module) {
+  // Iterate the keys in the module
+  var modules = Mason.modules,
+      key;
+  for (key in module) {
+    if (module.hasOwnProperty(key)) {
+      // Set each function to the static modules property (overloads pre-existing methods)
+      modules[key] = module[key];
+    }
+  }
+
+  // TODO: Return this for a fluent interface
+};
+
+// TODO: Mason.removeModule
+
+// TODO: Mason.clone?
+
+// Proof of concept (Simple): Make a button fancier
+Mason.addModule({
+  'button': function (buttonNode) {
+    // Create the button
+    var button = document.createElement('button'),
+        childNodes = buttonNode.childNodes;
+    button.setAttribute('style', 'background: black; border: 0; color: white; box-shadow: 0px 0px 5px 5px skyblue; margin: 10px; cursor: pointer;');
+
+    // If there are any children
+    if (childNodes !== undefined) {
+      button.appendChild(Mason(childNodes));
+    }
+
+    return button;
+  }
+});
+
+// Proof of concept (Advanced): Make a new foray of elements
+// Proof of concept (Advanced): Add in new event triggers corresponding to the UI element
 
 // Get the insertion area
 var insertArea = document.getElementById('insertArea'),
