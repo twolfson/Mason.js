27a1fe6ee4c91808fae41ba8cf6f3459dd4d301b
Todd Wolfson <todd@twolfson.com>
2012-04-04 22:55:04 -0700

    Beginning to make legitimate tests

--- src-test/Mason.test.js
+++ src-test/Mason.test.js
@@ -1,31 +1,37 @@
-{
-  'Mason': {
-    'can parse an XML string': '',
-    'can parse an HTML fragment': '',
-    'can parse an array of HTMLNodes/Objects': '',
-    'can parse an HTMLNode/Object': '',
-  },
-  'if a method doesnt exist': {
-    'should gracefully degrade to normal': ''
-  },
-  'if a method doesnt exist': {
-    'and the tag normally does not exist': {
-      'should gracefully degrade to normal': ''
-    }
-  },
-  'an onclick attribute in HTML on a normal tag': {
-    'should stay in rendering': {
-      'and work properly after': ''
-    }
-  },
-  'an onclick attribute in HTML on a custom tag': {
-    'should stay in rendering': {
-      'and work properly after': ''
-    }
-  },
-  'when use modules is disabled': {
-    'and a would-be custom tag is being rendered': {
-      'it is rendered as a normal tag': ''
-    }
-  }
-}
+describe('Mason', function () {
+  it('should be an object', function () {
+    assert(true);
+  });
+});
+
+// {
+  // 'Mason': {
+    // 'can parse an XML string': '',
+    // 'can parse an HTML fragment': '',
+    // 'can parse an array of HTMLNodes/Objects': '',
+    // 'can parse an HTMLNode/Object': '',
+  // },
+  // 'if a method doesnt exist': {
+    // 'should gracefully degrade to normal': ''
+  // },
+  // 'if a method doesnt exist': {
+    // 'and the tag normally does not exist': {
+      // 'should gracefully degrade to normal': ''
+    // }
+  // },
+  // 'an onclick attribute in HTML on a normal tag': {
+    // 'should stay in rendering': {
+      // 'and work properly after': ''
+    // }
+  // },
+  // 'an onclick attribute in HTML on a custom tag': {
+    // 'should stay in rendering': {
+      // 'and work properly after': ''
+    // }
+  // },
+  // 'when use modules is disabled': {
+    // 'and a would-be custom tag is being rendered': {
+      // 'it is rendered as a normal tag': ''
+    // }
+  // }
+// }
