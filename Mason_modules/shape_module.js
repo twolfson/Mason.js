426de014e75a435ea48e52a5d6e1073b93fe62a2
Todd Wolfson <todd@twolfson.com>
2012-04-12 00:22:31 -0700

    Shape module is working as wanted for testing

--- Mason_modules/shape_module.js
+++ Mason_modules/shape_module.js
@@ -1,58 +1,72 @@
 (function () {
-Mason.addModule('shape', function (shape) {
-  // Create our shape
-  var state = {'color': 'red', 'shape': 'square'},
-      retShape = document.createElement('div'),
-      attrs = shape.attributes,
+Mason.addModule('square', function (square) {
+  // Create our square
+  var state = {'color': 'red'},
+      retSquare = document.createElement('div'),
+      $retSquare = $(retSquare),
+      attrs = square.attributes,
       attr,
       i = 0,
       len = attrs.length;
 
-  // By default, the shape is a square
-  retShape.className = 'shape';
+  // Set up all of the attribute
+  Mason.setAttributes(retSquare, square);
 
-  // Iterate the attributes of the shape
+  // Set the styles for the square
+  var squareStyle = retSquare.style;
+  squareStyle.width = '100px';
+  squareStyle.height = '100px';
+  squareStyle.border = '1px solid #000';
+
+  // Set the value as the state
+  retSquare.value = state;
+
+  // Iterate the attributes of the square
   for (; i < len; i++) {
     attr = attrs[i];
 
-    switch (attr.nodeName) {
-      case 'color':
-        state.color = attr.nodeValue;
-        break;
-      case 'shape':
-        state.shape = attr.nodeValue;
-        break;
+    if (attr.nodeName === 'color') {
+      state.color = attr.nodeValue;
     }
   }
 
-  // Render the shape
+  // Render the square
   function render() {
     // If this is a color attribute, set one
-    retShape.style.background = state.color;
-
-    // If this is a shape attribute, set it as the class
-    retShape.className = 'shape ' + state.shape;
+    squareStyle.background = state.color;
   }
   render();
 
   // Set up custom methods
-  retShape.color = function (color) {
+  function setColor(color) {
+    var curColor = state.color;
+
+    // If the new color is different from current color
+    if (color !== curColor) {
+      // Update the state
+      state.color = color;
+
+      // Fire a colorchange event
+      $retSquare.trigger('colorchange', color);
+
+      // Render the shape
+      render();
+    }
+  }
+
+  retSquare.color = function (color) {
     // If this is a getter request, return the current color
     if (color === undefined) {
       return state.color;
     }
 
     // Otherwise, update the color
-    // TODO: Fire colorchange event
-    state.color = color;
-    render();
-  }
-  retShape.shape = function (shape) {
-    // TODO: Fire colorchange event
-    state.color = color;
-    render();
+    setColor(color);
   }
 
-  return retShape;
+  // Set up a custom event listener for setColor events
+  $retSquare.on('setcolor', setColor);
+
+  return retSquare;
 });
 }());
