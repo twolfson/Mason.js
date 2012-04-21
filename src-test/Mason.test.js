c44dea27fe8721cf731d940013f70a6b320574d5
Todd Wolfson <todd@twolfson.com>
2012-04-21 14:39:44 -0700

    More tests

--- src-test/Mason.test.js
+++ src-test/Mason.test.js
@@ -48,31 +48,110 @@ suite.addBatch({
   'can parse an HTMLNode/Object': ''
 });
 
+// First batch - Part Two
+suite.addBatch({
+  'A standard non-module tag (&lt;p&gt;)': {
+    topic: function () {
+      return '<p id="hello">World</p>';
+    },
+    'parsed by Mason': {
+      topic: function (tag) {
+        return Mason(tag);
+      },
+      // TODO: Get a properly working 'before' which cleans out past items from the sandbox
+      'returns an item which functions normally': function (docFrag) {
+        var sandbox = document.getElementById('TESTsandbox');
+        sandbox.appendChild(docFrag);
+
+        var tag = document.getElementById('hello');
+        assert(tag);
+        assert(tag.nodeName.toLowerCase() === 'p');
+        assert(tag.innerHTML === 'World');
+      }
+    }
+  },
+  'A non-standard non-module tag (&lt;dance&gt;)': {
+    topic: function () {
+      return '<dance id="everybody">dance</dance>';
+    },
+    'parsed by Mason': {
+      topic: function (tag) {
+        return Mason(tag);
+      },
+      'returns an item which functions normally': function (docFrag) {
+        var sandbox = document.getElementById('TESTsandbox');
+        sandbox.appendChild(docFrag);
+
+        var tag = document.getElementById('everybody');
+        assert(tag);
+        assert(tag.nodeName.toLowerCase() === 'dance');
+        assert(tag.innerHTML === 'dance');
+      }
+    }
+  },
+  'A standard module tag (&lt;table&gt;)': {
+    topic: function () {
+      Mason.addModule('table', function () {
+        // TODO: Write out module
+      });
+      return '<table id="myTable">round</table>';
+    },
+    'parsed by Mason': {
+      topic: function (tag) {
+        return Mason(tag);
+      },
+      'returns an item which functions normally': function (docFrag) {
+        var sandbox = document.getElementById('TESTsandbox');
+        sandbox.appendChild(docFrag);
+
+        var tag = document.getElementById('myTable');
+        assert(tag);
+        assert(tag.nodeName.toLowerCase() === 'table');
+        assert(tag.innerHTML === 'round');
+      }
+    }
+  },
+  'A non-standard module tag (&lt;spring&gt;)': {
+    topic: function () {
+      Mason.addModule('spring', function () {
+        // TODO: Write out module
+      });
+      return '<spring id="sproing">sprung</spring>';
+    },
+    'parsed by Mason': {
+      topic: function (tag) {
+        return Mason(tag);
+      },
+      'returns an item which functions normally': function (docFrag) {
+        var sandbox = document.getElementById('TESTsandbox');
+        sandbox.appendChild(docFrag);
+
+        var tag = document.getElementById('sproing');
+        assert(tag);
+        assert(tag.nodeName.toLowerCase() === 'spring');
+        assert(tag.innerHTML === 'sprung');
+      }
+    }
+  }
+});
+
 // Second batch: Intermediate
 suite.addBatch({
-  // 'if a method doesnt exist': {
-    // 'should gracefully degrade to normal': ''
-  // },
-  // 'if a method doesnt exist': {
-    // 'and the tag normally does not exist': {
-      // 'should gracefully degrade to normal': ''
-    // }
-  // },
-  // 'an onclick attribute in HTML on a normal tag': {
-    // 'should stay in rendering': {
-      // 'and work properly after': ''
-    // }
-  // },
-  // 'an onclick attribute in HTML on a custom tag': {
-    // 'should stay in rendering': {
-      // 'and work properly after': ''
-    // }
-  // },
-  // 'when use modules is disabled': {
-    // 'and a would-be custom tag is being rendered': {
-      // 'it is rendered as a normal tag': ''
-    // }
-  // }
+  'an onclick attribute in HTML on a normal tag': {
+    'should stay in rendering': {
+      'and work properly after': ''
+    }
+  },
+  'an onclick attribute in HTML on a custom tag': {
+    'should stay in rendering': {
+      'and work properly after': ''
+    }
+  },
+  'when use modules is disabled': {
+    'and a would-be custom tag is being rendered': {
+      'it is rendered as a normal tag': ''
+    }
+  }
 });
 
 // Third batch: Advanced
@@ -80,8 +159,9 @@ suite.addBatch({
   'A custom module (square)': {
     'when used': {
       topic: function () {
-        // TODO: Include some line breaks and carriage returns for realism
-        var squareStr = '<square id="TESTsquare" color="blue"></square>',
+        var squareStr = '<square id="TESTsquare" color="blue">\
+        \
+        </square>',
             docFrag = Mason(squareStr);
         sandbox.appendChild(docFrag);
         var square = document.getElementById('TESTsquare');
